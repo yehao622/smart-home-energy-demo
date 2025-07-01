b19f55ebab80a28e49bd75fa652bf65e769e5a0e
yehao622 <yeahaur@gmail.com>
2025-07-01 14:41:32 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ setupErrorHandling();
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { DEMO_MODE } from './config/productionDemo.js'
 
 createApp(App).mount('#app')
